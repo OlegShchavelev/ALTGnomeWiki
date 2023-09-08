5bae6de2d8cc1bad30d2c442b5badfc362c4d277
Anton Palgunov <Toxblh@users.noreply.github.com>
2023-09-08 23:51:59 +0000

    Add .prettierrc config

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,133 +1,131 @@
-import { defineConfig } from "vitepress";
-import { telegram } from "./icons";
-import { addPlugins } from "./theme/plugins/markdown";
+import { defineConfig } from 'vitepress'
+import { telegram } from './icons'
+import { addPlugins } from './theme/plugins/markdown'
 
-export const META_DESCRIPTION =
-  "Свободная WIKI по операционной системе ALT Regular Gnome";
+export const META_DESCRIPTION = 'Свободная WIKI по операционной системе ALT Regular Gnome'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/ALTRegularGnomeWiki/",
-  srcDir: "./docs",
-  title: "ALT Community",
+  base: '/ALTRegularGnomeWiki/',
+  srcDir: './docs',
+  title: 'ALT Community',
   description: META_DESCRIPTION,
   themeConfig: {
-    logo: "/logo.png",
+    logo: '/logo.png',
     search: {
-      provider: "local",
+      provider: 'local',
       options: {
         locales: {
           root: {
             translations: {
               button: {
-                buttonText: "Поиск",
-                buttonAriaLabel: "Поиск",
+                buttonText: 'Поиск',
+                buttonAriaLabel: 'Поиск'
               },
               modal: {
-                noResultsText: "Нет результатов по запросу",
-                resetButtonTitle: "Сбросить",
+                noResultsText: 'Нет результатов по запросу',
+                resetButtonTitle: 'Сбросить',
                 footer: {
-                  selectText: "для выбора",
-                  navigateText: "для навигации",
-                  closeText: "закрыть",
-                },
-              },
-            },
-          },
-        },
-      },
+                  selectText: 'для выбора',
+                  navigateText: 'для навигации',
+                  closeText: 'закрыть'
+                }
+              }
+            }
+          }
+        }
+      }
     },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: "Главная", link: "/" },
-      { text: "Документация", link: "/wiki" },
+      { text: 'Главная', link: '/' },
+      { text: 'Документация', link: '/wiki' },
       {
-        text: "Пролог",
+        text: 'Пролог',
         items: [
-          { text: "О проекте", link: "/about" },
-          { text: "Возможности VitePress", link: "/vitepress" },
-        ],
-      },
+          { text: 'О проекте', link: '/about' },
+          { text: 'Возможности VitePress', link: '/vitepress' }
+        ]
+      }
     ],
     socialLinks: [
       {
         icon: {
-          svg: telegram,
+          svg: telegram
         },
-        link: "https://t.me/alt_gnome",
-      },
+        link: 'https://t.me/alt_gnome'
+      }
     ],
     sidebar: [
       {
-        text: "Популярное",
+        text: 'Популярное',
         items: [
-          { text: "Быстрый старт", link: "/quick-start" },
+          { text: 'Быстрый старт', link: '/quick-start' },
           {
-            text: "Установка и обновление программ",
+            text: 'Установка и обновление программ',
             items: [
-              { text: "Blender", link: "/blender" },
-              { text: "Chromium", link: "/chromium" },
-              { text: "Codium", link: "/codium" },
-              { text: "Console", link: "/console" },
-              { text: "Eog", link: "/eog" },
-              { text: "Epm", link: "/epm" },
-              { text: "Evince", link: "/evince" },
-              { text: "Extensions", link: "/extensions" },
-              { text: "Figma", link: "/figma" },
-              { text: "Firefox", link: "/firefox" },
-              { text: "Flatpak", link: "/flatpak" },
-              { text: "Git", link: "/git" },
-              { text: "GnomeSoftware", link: "/gnome-software" },
-              { text: "Libreoffice", link: "/libreoffice" },
-              { text: "Loupe", link: "/loupe" },
-              { text: "Neofetch", link: "/neofetch" },
-              { text: "Nodejs", link: "/nodejs" },
-              { text: "Nvidia", link: "/nvidia" },
-              { text: "Opera", link: "/opera" },
-              { text: "Steam", link: "/steam" },
-              { text: "Telegram", link: "/telegram" },
-              { text: "Viber", link: "/viber" },
-              { text: "YandexBrowser", link: "/yandex-browser" },
+              { text: 'Blender', link: '/blender' },
+              { text: 'Chromium', link: '/chromium' },
+              { text: 'Codium', link: '/codium' },
+              { text: 'Console', link: '/console' },
+              { text: 'Eog', link: '/eog' },
+              { text: 'Epm', link: '/epm' },
+              { text: 'Evince', link: '/evince' },
+              { text: 'Extensions', link: '/extensions' },
+              { text: 'Figma', link: '/figma' },
+              { text: 'Firefox', link: '/firefox' },
+              { text: 'Flatpak', link: '/flatpak' },
+              { text: 'Git', link: '/git' },
+              { text: 'GnomeSoftware', link: '/gnome-software' },
+              { text: 'Libreoffice', link: '/libreoffice' },
+              { text: 'Loupe', link: '/loupe' },
+              { text: 'Neofetch', link: '/neofetch' },
+              { text: 'Nodejs', link: '/nodejs' },
+              { text: 'Nvidia', link: '/nvidia' },
+              { text: 'Opera', link: '/opera' },
+              { text: 'Steam', link: '/steam' },
+              { text: 'Telegram', link: '/telegram' },
+              { text: 'Viber', link: '/viber' },
+              { text: 'YandexBrowser', link: '/yandex-browser' }
             ],
-            collapsed: true,
+            collapsed: true
           },
-          { text: "Запись на DVD и USB Flash", link: "/getting-started" },
+          { text: 'Запись на DVD и USB Flash', link: '/getting-started' },
 
-          { text: "Алтай", link: "/altai" },
-        ],
-      },
+          { text: 'Алтай', link: '/altai' }
+        ]
+      }
     ],
     editLink: {
-      pattern:
-        "https://github.com/OlegShchavelev/ALTRegularGnomeWiki/edit/main/docs/:path",
-      text: "Предложить изменения на этой странице",
+      pattern: 'https://github.com/OlegShchavelev/ALTRegularGnomeWiki/edit/main/docs/:path',
+      text: 'Предложить изменения на этой странице'
     },
     lastUpdated: {
-      text: "Последнее обновление",
+      text: 'Последнее обновление',
       formatOptions: {
-        dateStyle: "medium",
-        timeStyle: "medium",
-      },
+        dateStyle: 'medium',
+        timeStyle: 'medium'
+      }
     },
-    returnToTopLabel: "Наверх",
-    sidebarMenuLabel: "Меню",
+    returnToTopLabel: 'Наверх',
+    sidebarMenuLabel: 'Меню',
     sponsor: {
       message:
-        "Данный сервис является Open-Source проектом и его поддержка и развитие зависит от пожертвований.",
-      linkText: "Поддержать проект!",
+        'Данный сервис является Open-Source проектом и его поддержка и развитие зависит от пожертвований.',
+      linkText: 'Поддержать проект!'
     },
     docFooter: {
-      prev: "Предыдущая страница",
-      next: "Следующая страница",
+      prev: 'Предыдущая страница',
+      next: 'Следующая страница'
     },
-    outlineTitle: "Оглавление",
+    outlineTitle: 'Оглавление',
     footer: {
-      copyright: "Copyright © 2023 ALT Regular Gnome Community",
-    },
+      copyright: 'Copyright © 2023 ALT Regular Gnome Community'
+    }
   },
   markdown: {
     config(md) {
-      addPlugins(md);
-    },
-  },
-});
+      addPlugins(md)
+    }
+  }
+})

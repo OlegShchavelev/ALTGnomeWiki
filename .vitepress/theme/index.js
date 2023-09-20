526fe0e56cc8090104a327038a963960d2afacf8
Олег Щавелев <o.shchavelev@yandex.ru>
2023-09-20 10:50:51 +0300

    Добавлено: imageViewer

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,9 +1,12 @@
 // https://vitepress.dev/guide/custom-theme
 import { h } from 'vue'
+import { useRoute } from 'vitepress';
 import Theme from 'vitepress/theme'
+import imageViewer from 'vitepress-plugin-image-viewer';
 
 import './style.css'
 import './custom.css'
+import 'viewerjs/dist/viewer.min.css';
 
 export default {
   extends: Theme,
@@ -14,5 +17,11 @@ export default {
   },
   enhanceApp({ app, router, siteData }) {
   
+  },
+  setup() {
+    // Get route
+    const route = useRoute();
+    // Using
+    imageViewer(route);
   }
 }

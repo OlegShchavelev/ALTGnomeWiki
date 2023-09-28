2fb250e4abf3cf10119fa050dab1152040cb9764
Олег Щавелев <o.shchavelev@yandex.ru>
2023-09-28 04:56:42 +0300

    Добавлено: Яндекс Метрика

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -13,7 +13,23 @@ export default defineConfig({
     [
       'meta', 
       { name: 'yandex-verification', content: '6ef3a36c3d09e43e' }
-    ]
+    ],
+    [
+      'script',
+      {},
+      `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   
+      ym(95081395, "init", {
+           clickmap:true,
+           trackLinks:true,
+           accurateTrackBounce:true,
+           webvisor:true
+      });`,
+    ],
   ],
   themeConfig: {
     logo: '/logo.png',

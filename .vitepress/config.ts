1892d8e9a5b01dde826da59a85cdf35f4645238f
Олег Щавелев <o.shchavelev@yandex.ru>
2023-12-03 04:21:34 +0300

    Обновление: Проблема #58 и #52

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -7,9 +7,41 @@ import kbd from 'markdown-it-kbd'
 import taskLists from 'markdown-it-task-lists'
 import { rewrites } from './paths'
 
+export const META_DESCRIPTION = 'Свободная WIKI по операционной системе ALT Regular Gnome'
+
 export default defineConfig({
-  title: "ALT Regular Gnome",
-  description: "открытое сообщество пользователей операционной системы ALT Regular Gnome",
+  vite: { 
+    ssr: { 
+      noExternal: [ 
+        '@nolebase/vitepress-plugin-enhanced-readabilities', 
+      ], 
+    }, 
+  }, 
+  title: seo.SITE_TITLE,
+  titleTemplate: ':title' + seo.SITE_TITLE_SEPARATOR + seo.SITE_TITLE,
+  description: META_DESCRIPTION,
+  head: [
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+    ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }],
+    ['meta', { name: 'theme-color', content: '#62a0ea' }],
+    ['meta', { name: 'yandex-verification', content: '6ef3a36c3d09e43e' }],
+    [
+      'script',
+      {},
+      `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+   
+      ym(95081395, "init", {
+           clickmap:true,
+           trackLinks:true,
+           accurateTrackBounce:true,
+           webvisor:true
+      });`,
+    ],
+  ],
   lang: 'ru-RU',
   srcDir: './docs',
   sitemap: {

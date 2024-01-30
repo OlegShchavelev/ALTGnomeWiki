2b93a30e366b38c91cf5b5b30154005f2d7de3be
Олег Щавелев <o.shchavelev@yandex.ru>
2024-01-30 05:00:12 +0300

    Обновлено: PortProton, навигация

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -141,6 +141,7 @@ export default defineConfig({
               { text: 'OnlyOffice', link: '/onlyoffice' },
               { text: 'Opera', link: '/opera' },
               { text: 'Penpot Desktop', link: '/penpot-desktop' },
+              { text: 'Port Proton', link: '/port-proton' },
               { text: 'Р7-Офис', link: '/r7-office' },
               { text: 'Ресурсы', link: '/resources' },
               { text: 'Сканер документов', link: '/simple-scan' },

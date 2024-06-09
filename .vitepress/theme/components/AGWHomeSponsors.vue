c012e5696363a6b24be8d6d172545a57132d10ec
Олег Щавелев <o.shchavelev@yandex.ru>
2024-06-10 02:33:32 +0300

    Обновлено: Спонсорская кнопка, шаблон AGWHomeSponsors

--- .vitepress/theme/components/AGWHomeSponsors.vue
+++ .vitepress/theme/components/AGWHomeSponsors.vue
@@ -2,10 +2,18 @@
 import { VPHomeSponsors } from 'vitepress/theme'
 import { sponsors } from '../../../_data/sponsors';
 
-const { message, data } = defineProps({
+const { message, actionLink, actionText, data } = defineProps({
   message: {
     type: String,
     default: 'Данный сервис является Open-Source проектом и его поддержка и развитие зависит только от нашей совместной активности.'
+  },
+  actionText: {
+    type: String,
+    default: 'Поддержать проект ALT Gnome'
+  },
+  actionLink: {
+    type: String,
+    default: 'https://boosty.to/alt_gnome'
   },
   data: {
     type: Object,
@@ -18,7 +26,6 @@ const { message, data } = defineProps({
 </script>
 
 <template>
-    <VPHomeSponsors v-if="sponsors"
-        :message="message"
-        :data="sponsors" />
+  <VPHomeSponsors v-if="sponsors" :message="message" :actionText="actionText" :actionLink="actionLink"
+    :data="sponsors" />
 </template>

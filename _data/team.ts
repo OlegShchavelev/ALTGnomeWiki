65dd0afb078b7a6bfdf49576d2e8209de35d05c3
Ampernic <ampernic@inbox.ru>
2024-06-27 21:55:35 +0300

    Исправление ошибок: workflow и team.ts

--- _data/team.ts
+++ _data/team.ts
@@ -15,11 +15,11 @@ const leader = 'Олег Щавелев'
 const limit = 6
 const developersSection = {
   title: 'Разработчики',
-  description: 'Люди принимающие участие в активном не только в расширении базы знаний, но и в улучшении ее функционала'
+  description: 'Люди, которые активно участвуют не только в расширении базы знаний, но и в улучшении её функционала.'
 }
 const membersSection = {
   title: 'Авторы',
-  description: 'Люди принимающие участие исправлении и написании новых статей' 
+  description: 'Люди, которые участвуют в улучшении и создании новых статей.' 
 }
 
 export { homeSorting, teamSorting, leader, limit, membersSection, developersSection }

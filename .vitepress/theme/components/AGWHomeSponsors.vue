60d599f587255757d267030b70b29d227a525fa5
Ampernic <ampernic@inbox.ru>
2024-07-11 22:39:26 +0300

    Перенос блока спонсоров в frontmatter

--- .vitepress/theme/components/AGWHomeSponsors.vue
+++ .vitepress/theme/components/AGWHomeSponsors.vue
@@ -1,36 +1,21 @@
 <script setup lang="ts">
+import { useData } from 'vitepress'
 import { VPHomeSponsors } from 'vitepress/theme'
-import { sponsors } from '../../../_data/sponsors'
 
-const { message, actionLink, actionText, data } = defineProps({
-  message: {
-    type: String,
-    default:
-      'Данный сервис является Open-Source проектом и его поддержка и развитие зависит только от нашей совместной активности.'
-  },
-  actionText: {
-    type: String,
-    default: 'Поддержать проект ALT Gnome'
-  },
-  actionLink: {
-    type: String,
-    default: 'https://boosty.to/alt_gnome'
-  },
-  data: {
-    type: Object,
-    default: () => {
-      return sponsors ?? []
-    }
-  }
-})
+const { frontmatter } = useData()
+const actionText = frontmatter.value.sponsors.donationtext ?? null
+const actionLink = frontmatter.value.sponsors.donationlink ?? null
+const message = frontmatter.value.sponsors.introtext ?? null
+const data = frontmatter.value.sponsors.collections ?? {}
+
 </script>
 
 <template>
   <VPHomeSponsors
-    v-if="sponsors"
-    :message="message"
+    v-if="data"
     :actionText="actionText"
     :actionLink="actionLink"
-    :data="sponsors"
+    :message="message"
+    :data="data"
   />
 </template>
--- docs/index.md
+++ docs/index.md
@@ -34,4 +34,38 @@ features:
       light: /users-light.svg
     title: Открытое сообщество
     details: Путеводитель в мир открытого программного обеспечения.
+
+sponsors:
+  introtext: Данный сервис является CПО-проектом и его поддержка и развитие зависит только от нашей совместной активности.
+  donationtext: 'Поддержать проект ALT Gnome'
+  donationlink: 'https://boosty.to/alt_gnome'
+  collections:
+    - tier: Организационная структура
+      size: big
+      items: 
+        - name: ALT Linux Team
+          img: ./alt-linux-team.png
+          url: https://packages.altlinux.org/ru/sisyphus
+        - name: BaseALT
+          img: ./basealt.png
+          url: https://www.basealt.ru
+    - tier: Технические партнеры
+      size: big
+      items:
+        - name: Beget
+          img: ./beget.png
+          url: https://beget.com
+    - tier: Товарищи по цеху Vitepress
+      size: big
+      items: 
+        - name: MODX Pro
+          img: ./modx.svg
+          url: https://docs.modx.pro/
+        - name: IT WIKI
+          img: ./logo_for_friends_horizontal.svg
+          url: https://it-wiki.com.ru/
+        - name: Nólëbase
+          img: ./nolebase-logo.svg
+          url: https://nolebase.ayaka.io/
 ---
+

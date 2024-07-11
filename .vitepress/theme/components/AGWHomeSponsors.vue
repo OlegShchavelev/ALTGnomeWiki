23db844905345b991f73ffdbc972b08ca22b9269
Ampernic <ampernic@inbox.ru>
2024-07-11 23:00:07 +0300

    Мелкие правки шаблона `HomeSponsors`

--- .vitepress/theme/components/AGWHomeSponsors.vue
+++ .vitepress/theme/components/AGWHomeSponsors.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useData } from 'vitepress'
 import { VPHomeSponsors } from 'vitepress/theme'
 
 const { frontmatter } = useData()
-const actionText = frontmatter.value.sponsors.donationtext ?? null
-const actionLink = frontmatter.value.sponsors.donationlink ?? null
-const message = frontmatter.value.sponsors.introtext ?? null
-const data = frontmatter.value.sponsors.collections ?? {}
-
+const props = computed(() => frontmatter.value.sponsors ?? {} )
 </script>
 
 <template>
   <VPHomeSponsors
-    v-if="data"
-    :actionText="actionText"
-    :actionLink="actionLink"
-    :message="message"
-    :data="data"
+    v-if="props.collections"
+    :actionText="props.donationtext"
+    :actionLink="props.donationlink"
+    :message="props.introtext"
+    :data="props.collections"
   />
 </template>

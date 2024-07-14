7bb014992c4135e65d86f356aa6feac740b2c0a0
Олег Щавелев <o.shchavelev@yandex.ru>
2024-07-14 04:01:31 +0300

    component: add scoped styles for AGWHomeSponsors

--- .vitepress/theme/components/AGWHomeSponsors.vue
+++ .vitepress/theme/components/AGWHomeSponsors.vue
@@ -4,15 +4,26 @@ import { useData } from 'vitepress'
 import { VPHomeSponsors } from 'vitepress/theme'
 
 const { frontmatter } = useData()
-const props = computed(() => frontmatter.value.sponsors ?? {} )
+const props = computed(() => frontmatter.value.sponsors ?? {})
 </script>
 
 <template>
-  <VPHomeSponsors
-    v-if="props.collections"
-    :actionText="props.donationtext"
-    :actionLink="props.donationlink"
-    :message="props.introtext"
-    :data="props.collections"
-  />
+  <VPHomeSponsors class="VPHomeSponsors" v-if="props.collections" :actionText="props.donationtext"
+    :actionLink="props.donationlink" :message="props.introtext" :data="props.collections" />
 </template>
+
+<style scoped>
+
+.VPHomeSponsors {
+  margin-top: 0;
+}
+
+.VPHomeSponsors:deep(.message) {
+  max-width: 800px;
+}
+
+.VPHomeSponsors:deep(.VPSponsorsGrid.big .vp-sponsor-grid-image) {
+  max-width: 170px;
+  max-height: 120px;
+}
+</style>

cc5c141057cc045059e85ddd3ed48d7da24e661d
cryals <gghj31621@gmail.com>
2024-07-15 10:57:21 +0300

    Добавлено: GNOME Extensions

--- _data/navigations.ts
+++ _data/navigations.ts
@@ -158,6 +158,7 @@ export const docs = [
           { text: 'Погода', link: '/gnome-weather' },
           { text: 'Подключения', link: '/gnome-connections' },
           { text: 'Р7-Офис', link: '/r7-office' },
+          { text: 'Pасширения', link: '/gnome-shell-extensions' },
           { text: 'Ресурсы', link: '/resources' },
           { text: 'Сканер документов', link: '/simple-scan' },
           { text: 'Тест скорости', link: '/speedtest' },

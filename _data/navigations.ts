ee053177922139b28b9fb32fabc9c6a031ec89c5
Constacalm <163557834+Constacalm@users.noreply.github.com>
2025-07-19 17:40:31 +0300

    Update _data/navigations.ts
    Co-authored-by: Sultaniiazov David <x1z53@yandex.ru>

--- _data/navigations.ts
+++ _data/navigations.ts
@@ -45,7 +45,7 @@ export const sidebar: Record<string, DefaultTheme.Sidebar> = {
           {
             text: 'Безопасность',
             link: '/security',
-	    items: [
+            items: [
 		{ text: 'Общие принципы', link: '/security/concepts' },
 		{ text: 'Некоторые вопросы безопасности', link: '/security/some_security_problems' },
 		{ text: 'Двухфакторная аутентификация', link: '/security/u2f' }

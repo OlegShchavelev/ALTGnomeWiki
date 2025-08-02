ecdba9f9f5f2a1859c98037af5b38906b0870143
Oleg Shchavelev <o.shchavelev@yandex.ru>
2025-08-02 18:28:29 +0300

    feat: improve domain name extraction by removing www domain

--- .vitepress/theme/composables/link.ts
+++ .vitepress/theme/composables/link.ts
@@ -1,5 +1,5 @@
 export const getDomainName = (url: any) => url.match(/\/\/([^/]+)\//)[1].replace('www.', '')
 
 export const extractDomain = (url: string) => {
-  return new URL(url).hostname
+  return new URL(url).hostname.replace(/^www\./i, '')
 }

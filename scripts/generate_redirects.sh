6baa4e2036e1e41a53b3c1d8fea35864aa2f6b0a
x1z53 <x1z53@alt-gnome.ru>
2025-11-04 13:13:12 +0300

    scripts(generate_redirects): add custom redirects functionality

--- scripts/generate_redirects.sh
+++ scripts/generate_redirects.sh
@@ -8,7 +8,16 @@ fi
 
 output_file="$(pwd)/$1"
 
+custom_redirects=(
+    "/Synching/ /syncthing/ 301"
+    "/bottle/ /bottles/ 301"
+)
+
 > "$output_file"
+
+for redirect in "${custom_redirects[@]}"; do
+    echo "$redirect" >> "$output_file"
+done
 
 cd docs/ru
 
